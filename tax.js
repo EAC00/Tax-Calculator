51776d1d4e45496ac8157444e2789dbd53cf3269
EAC00 <143907088+EAC00@users.noreply.github.com>
2023-10-26 16:05:57 -0400

    Add files via upload

---
+++ tax.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const $ = selector => document.querySelector(selector);
+
+const getErrorMsg = lbl => `${lbl} must be a valid number greater than zero and less than 100.`;
+
+const focusAndSelect = selector => {
+    const elem = $(selector);
+    elem.focus();
+    elem.select();
+};
+
+const calculateTax = (subtotal, taxRate) => {
+    const taxAmount = subtotal * taxRate / 100;
+    return taxAmount;
+};
+
+const processEntries = () => {
+    const sales = parseFloat($("#sales").value);
+    const tax = parseFloat($("#tax").value);
+
+    if (isNaN(sales) || sales <= 0) {
+        alert(getErrorMsg("Sales Amount"));
+        focusAndSelect("#sales");
+    } else if (isNaN(tax) || tax <= 0 || tax >= 100) {
+        alert(getErrorMsg("Tax Percent"));
+        focusAndSelect("#tax");
+    } else {
+        $("#total").value = (sales + calculateTax(sales, tax)).toFixed(2);
+    }
+};
+
+var clearEntries = () => {
+    $("#sales").value = "";
+    $("#tax").value = "";
+    $("#total").value = "";
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    $("#calculate").addEventListener("click", processEntries);
+    $("#sales").focus();
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    $("#clear").addEventListener("click", clearEntries);
+});
